a01fce8bb27e423836ab8363badf38f40081c49d
Bernardo <tgavent3@gmail.com>
2023-03-31 11:09:50 +0100

    WebClient http response changes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,35 +1,44 @@
 import java.io.*
 import java.net.*
 
+
 fun main() {
-    try {
-        // establish a TCP connection with the server
-        print("Host:")
-        val clientHost = readln()
-        print("\nPort:")
-        val clientPort = readln().toInt()
-        val clientSocket = Socket(clientHost, clientPort)
-        print("\nCommand:")
-        val clientGet = readln()
-        // send an HTTP request for the HTML page to the server
-        val outToServer = PrintWriter(clientSocket.getOutputStream())
-        outToServer.println("$clientGet\r\nHost: $clientHost \r\n\r\n")
-        outToServer.flush()
-
-        // read and print the HTTP response from the server
-        val inFromServer = BufferedReader(InputStreamReader(clientSocket.getInputStream()))
-        var responseLine: String? = inFromServer.readLine()
-        val HTTP_response = responseLine!!.split(" ")[1]
-        while (responseLine != null) {
-            println(responseLine)
-            responseLine = inFromServer.readLine()
-        }
-
-        // close the socket and the input/output streams
-        inFromServer.close()
-        outToServer.close()
-        clientSocket.close()
+    val okResponses = listOf(200, 301)
+     try {
+        connect(okResponses)
     } catch (e: Exception) {
         e.printStackTrace()
     }
+}
+
+
+fun connect(okResponses: List<Int>) {
+    print("Host:")
+    val clientHost = readln()
+    print("\nPort:")
+    val clientPort = readln().toInt()
+    val clientSocket = Socket(clientHost, clientPort)
+    print("\nCommand:")
+    val clientGet = readln()
+    // send an HTTP request for the HTML page to the server
+    val outToServer = PrintWriter(clientSocket.getOutputStream())
+    outToServer.println("$clientGet\r\nHost: $clientHost \r\n\r\n")
+    outToServer.flush()
+
+    // read and print the HTTP response from the server
+    val inFromServer = BufferedReader(InputStreamReader(clientSocket.getInputStream()))
+    var responseLine: String? = inFromServer.readLine()
+    val HTTP_response = responseLine!!.split(" ")[1].toInt()
+    while (responseLine != null) {
+        println(responseLine)
+        responseLine = inFromServer.readLine()
+    }
+    println()
+    println()
+    if (HTTP_response in okResponses) {
+        inFromServer.close()
+        outToServer.close()
+        clientSocket.close()
+    }
+    else connect(okResponses)
 }
