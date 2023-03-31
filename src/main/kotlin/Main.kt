a1695b1d1990afa9bd7c2b6c2d4670495dffbf7d
antonio-paulino <antonioppaulino2@gmail.com>
2023-03-31 10:30:35 +0100

    WebClient update

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,50 +1,19 @@
 import java.io.*
 import java.net.*
 
-/*fun main() {
-    println("enter the server name")
-    val serverName = readln()
-    val port = 80
-
-    try {
-        // establish a TCP connection with the server
-        val clientSocket = Socket(serverName, port)
-
-        // send an HTTP GET request to the server
-        val outToServer = PrintWriter(clientSocket.getOutputStream())
-        outToServer.println("GET / HTTP/1.1\r\nHost: $serverName\r\n\r\n")
-        outToServer.flush()
-
-     ww   // read the HTTP response from the server
-        val inFromServer = BufferedReader(InputStreamReader(clientSocket.getInputStream()))
-        var responseLine: String? = inFromServer.readLine()
-        while (responseLine != null) {
-            println(responseLine)
-            responseLine = inFromServer.readLine()
-        }
-
-        // close the socket and the input/output streams
-        inFromServer.close()
-        outToServer.close()
-        clientSocket.close()
-    } catch (e: Exception) {
-        e.printStackTrace()
-    }
-}*/
-
 fun main() {
     try {
         // establish a TCP connection with the server
-
-
-
-
-
-        val clientSocket = Socket(readln(), 80)
-
+        println("Host:")
+        val clientHost = readln()
+        println("Port:")
+        val clientPort = readln().toInt()
+        val clientSocket = Socket(clientHost, clientPort)
+        println("Get:")
+        val clientGet = readln()
         // send an HTTP GET request for the HTML page to the server
         val outToServer = PrintWriter(clientSocket.getOutputStream())
-        outToServer.println("GET / HTTP/1.1\r\nHost: $(clientSocket. ) \r\n\r\n")
+        outToServer.println("$clientGet\r\nHost: $clientHost \r\n\r\n")
         outToServer.flush()
 
         // read and print the HTTP response from the server
